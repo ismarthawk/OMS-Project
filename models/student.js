430ace11bfd482fb1aa5a7bda08d06e3079a5716
ismarthawk <damodar.pulla@gmail.com>
2022-03-25 17:27:06 +0530

    worked on responsiveness

--- models/student.js
+++ models/student.js
@@ -1,85 +1,98 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.schema({
-    name: {
-        type: String,
-        required: true,
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  regNumber: {
+    type: Number,
+    required: true,
+    maxLength: 6,
+  },
+  rollNumber: {
+    type: Number,
+    required: true,
+    maxLength: 6,
+  },
+  year: {
+    type: Number,
+    required: true,
+    maxLength: 1,
+  },
+  branch: {
+    type: String,
+    enum: [
+      "Biotech",
+      "Chemical",
+      "Civil",
+      "CSE",
+      "ECE",
+      "EEE",
+      "Mechanical",
+      "MME",
+    ],
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    required: true,
+  },
+  Block: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Block",
+  },
+  roomNumber: {
+    type: String,
+  },
+  mail: {
+    type: String,
+    required: true,
+  },
+  mobileNumber: {
+    type: Number,
+    required: true,
+    maxLength: 10,
+  },
+  parentMobileNumber: {
+    type: Number,
+    required: true,
+    maxLength: 10,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  pendingOutings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Outing",
     },
-    regNumber: {
-        type: Number,
-        required: true,
-        maxLength: 6
+  ],
+  usedOutings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Outing",
     },
-    rollNumber: {
-        type: Number,
-        required: true,
-        maxLength: 6
+  ],
+  activeOuting: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Outing",
+  },
+  rejectedOutings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Outing",
     },
-    year: {
-        type: Number,
-        required: true,
-        maxLength: 1
-    },
-    branch: {
-        type: String,
-        enum:['Biotech','Chemical','Civil','CSE','ECE','EEE','Mechanical','MME'],
-        required: true,
-    },
-    gender: {
-        type: String,
-        enum: ['male', 'female'],
-        required: true,
-    },
-    Block: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Block',
-    },
-    roomNumber: {
-        type: String,
-    },
-    mail: {
-        type: String,
-        required: true,
-    },
-    mobileNumber: {
-        type: Number,
-        required: true,
-        maxLength: 10
-    },
-    parentMobileNumber: {
-        type: Number,
-        required: true,
-        maxLength: 10
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    pendingOutings: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Outing',
-    }],
-    usedOutings: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Outing',
-    }],
-    activeOuting: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Outing',
-    },
-    rejectedOutings: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Outing',
-    }],
-    inCampus: {
-        type: Boolean,
-        default: true,
-        required: true
-    }
-})
+  ],
+  inCampus: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+});
 
-
-
-const Student = mongoose.model('Student', studentSchema);
+const Student = mongoose.model("Student", studentSchema);
 
 module.exports = Student;
